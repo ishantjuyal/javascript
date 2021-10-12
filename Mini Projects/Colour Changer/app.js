8e4aff0bde85e5be17849f2f593d36b849d43439
Ishant Juyal <ishanjj61@gmail.com>
2021-10-12 17:33:42 +0530

    Changed CSS of Colour Changer

--- Mini Projects/Colour Changer/app.js
+++ Mini Projects/Colour Changer/app.js
@@ -1,19 +1,15 @@
-let colorArray = ["gold", "lightgreen", "cyan", "aqua", "orange", "black", "white", "silver", "yellow", "pink"];
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 let changeBtn = document.getElementById("change-btn");
 
 changeBtn.addEventListener('click', function (){
-    let container = document.getElementById("container");
-    let text = document.getElementById("text");
-    random = Math.floor(Math.random()*colorArray.length);
-    
-    container.style.background = colorArray[random];
-    text.textContent = colorArray[random];
-    
-    if(colorArray[random] === "black") {
-        text.style.color = "white";
-    }
-    else {
-        text.style.color = "black";    
-    }
+    let container = document.getElementsByTagName("BODY")[0];
+    container.style.background = "#" + ((1<<24)*Math.random() | 0).toString(16);
 })
