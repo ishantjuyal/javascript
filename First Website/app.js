04588212db8177dad89333657028eb85b7e3020b
Ishant Juyal <ishanjj61@gmail.com>
2021-10-16 16:24:19 +0530

    Moved some folders

--- First Website/app.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnavbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnavbar";
-    }
-}
